0da63f799e330868103af3b3fe303649e3f39bd4
Rahul Rahaman <rahulrahaman484@gmail.com>
2022-08-08 21:13:36 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -107,6 +107,7 @@ async def bot_start(event):  # sourcery skip: low-code-quality
             start_msg = f"Hey! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
                         \nYou can contact to my master from here.\"
+            buttons = None
     else:
         start_msg = "Hey Master!\
             \nHow can i help you ?"
